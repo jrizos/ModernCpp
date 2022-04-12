38cf7bfbc372d15cd6b548141925ed5d3caf96da
Jimmy Rizos <jimmy@rizosfamily.com>
2022-04-11 21:39:13 -0400

    Add HelloWorld

---
+++ HelloWorld/helloworld.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello C++ World" << std::endl;
+
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the c++ extension!"};
+    
+    for (const string& word: msg) 
+    {
+        cout << word << " ";
+    }
+    cout << endl;
+}
+
+// End of File
